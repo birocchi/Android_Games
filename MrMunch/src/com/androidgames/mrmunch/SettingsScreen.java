667a109c50e411aa60ee4258af17e254e225abc9
Anderson Birocchi <anderson.birocchi@gmail.com>
2011-07-16 11:50:45 -0300

    Settings screen finished

--- MrMunch/src/com/androidgames/mrmunch/SettingsScreen.java
+++ MrMunch/src/com/androidgames/mrmunch/SettingsScreen.java
@@ -24,6 +24,7 @@ public class SettingsScreen extends Screen {
         int len = events.size();
         for(int i=0; i<len; i++) {
         	TouchEvent event = events.get(i);
+        	
         	if (event.type == TouchEvent.TOUCH_UP) {
                 if (event.x < 64 && event.y > 416) {
                     if(Settings.soundEnabled)
@@ -32,7 +33,57 @@ public class SettingsScreen extends Screen {
                     return;
                 }
             }
-        	//TODO: Decide what to do//
+        	
+        	int y = g.getHeight()/2;
+        	if (event.type == TouchEvent.TOUCH_UP) {
+        		int x = 1*(g.getWidth()-20)/5 - 20;
+                if (event.x > x && event.x < x+20 && event.y > y && event.y < y+32) {
+                    if(Settings.soundEnabled)
+                        Assets.click.play(1);
+                    Settings.gameSpeed = 1;
+                    return;
+                }
+            }
+        	
+        	if (event.type == TouchEvent.TOUCH_UP) {
+        		int x = 2*(g.getWidth()-20)/5 - 20;
+        		if (event.x > x && event.x < x+20 && event.y > y && event.y < y+32) {
+                    if(Settings.soundEnabled)
+                        Assets.click.play(1);
+                    Settings.gameSpeed = 2;
+                    return;
+                }
+            }
+        	
+        	if (event.type == TouchEvent.TOUCH_UP) {
+        		int x = 3*(g.getWidth()-20)/5 - 20;
+        		if (event.x > x && event.x < x+20 && event.y > y && event.y < y+32) {
+                    if(Settings.soundEnabled)
+                        Assets.click.play(1);
+                    Settings.gameSpeed = 3;
+                    return;
+                }
+            }
+        	
+        	if (event.type == TouchEvent.TOUCH_UP) {
+        		int x = 4*(g.getWidth()-20)/5 - 20;
+        		if (event.x > x && event.x < x+20 && event.y > y && event.y < y+32) {
+                    if(Settings.soundEnabled)
+                        Assets.click.play(1);
+                    Settings.gameSpeed = 4;
+                    return;
+                }
+            }
+        	
+        	if (event.type == TouchEvent.TOUCH_UP) {
+        		int x = 5*(g.getWidth()-20)/5 - 20;
+        		if (event.x > x && event.x < x+20 && event.y > y && event.y < y+32) {
+                    if(Settings.soundEnabled)
+                        Assets.click.play(1);
+                    Settings.gameSpeed = 5;
+                    return;
+                }
+            }
         }
 	}
 
@@ -49,7 +100,7 @@ public class SettingsScreen extends Screen {
         		g.drawRect(i * (g.getWidth()-20)/5, g.getHeight()/2+13, 6, 6, Color.GREEN);
         	}
         	else
-        		g.drawText(g, ""+i, i * (g.getWidth()-20)/5 - 20 , g.getHeight()/2);
+        		g.drawText(g, ""+i, i * (g.getWidth()-30)/5 - 20 , g.getHeight()/2);
         }
         g.drawPixmap(Assets.buttons, 0, 416, 64, 64, 64, 64);
 	}
