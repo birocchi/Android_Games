2d0f500e13798c5ca8b0d3729b6f8c37ca5bfac1
Carlos Davi <davipmontenegro@gmail.com>
2011-09-02 00:11:58 -0300

    Created a single Class for the 3 HelpScreens

--- Book_Projects/ch06-mrnom/src/com/badlogic/androidgames/mrnom/HelpScreens.java
+++ Book_Projects/ch06-mrnom/src/com/badlogic/androidgames/mrnom/HelpScreens.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 import com.badlogic.androidgames.framework.Game;
 import com.badlogic.androidgames.framework.Graphics;
-import com.badlogic.androidgames.framework.Screen;
 import com.badlogic.androidgames.framework.Input.TouchEvent;
+import com.badlogic.androidgames.framework.Screen;
 
-public class HelpScreen3 extends Screen {
-    
-    public HelpScreen3(Game game) {
+public class HelpScreens extends Screen {
+	private int screenNumber = 1;
+	
+    public HelpScreens(Game game) {
         super(game);
     }
-    
+
     @Override
     public void update(float deltaTime) {
         List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
@@ -23,7 +24,10 @@ public class HelpScreen3 extends Screen {
             TouchEvent event = touchEvents.get(i);
             if(event.type == TouchEvent.TOUCH_UP) {
                 if(event.x > 256 && event.y > 416 ) {
-                    game.setScreen(new MainMenuScreen(game));
+                	screenNumber++;
+                	if(screenNumber>3){
+                		game.setScreen(new MainMenuScreen(game));
+                	}
                     if(Settings.soundEnabled)
                         Assets.click.play(1);
                     return;
@@ -34,28 +38,36 @@ public class HelpScreen3 extends Screen {
 
     @Override
     public void present(float deltaTime) {
-        Graphics g = game.getGraphics();      
-        g.drawPixmap(Assets.background, 0, 0);
-        g.drawPixmap(Assets.help3, 64, 100);
-        g.drawPixmap(Assets.buttons, 256, 416, 0, 128, 64, 64);
+    	Graphics g = game.getGraphics();
+    	g.drawPixmap(Assets.background, 0, 0);
+    	switch(screenNumber){
+    	case 1:
+    		g.drawPixmap(Assets.help1, 64, 100);
+    		g.drawPixmap(Assets.buttons, 256, 416, 0, 64, 64, 64);
+    		break;
+    	case 2:
+    		g.drawPixmap(Assets.help2, 64, 100);
+    		g.drawPixmap(Assets.buttons, 256, 416, 0, 64, 64, 64);
+    		break;
+    	case 3:
+    		g.drawPixmap(Assets.help3, 64, 100);
+    		g.drawPixmap(Assets.buttons, 256, 416, 0, 128, 64, 64);
+    		break;
+    	}
     }
 
     @Override
     public void pause() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void resume() {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void dispose() {
-        // TODO Auto-generated method stub
 
     }
-
 }
